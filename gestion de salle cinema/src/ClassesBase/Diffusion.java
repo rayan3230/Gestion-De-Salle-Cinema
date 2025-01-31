f7ccfbcb6648903f5eb16506f805d8343e01d572
rayan3230 <rayanemouzali3230@gmail.com>
2025-01-31 18:43:37 +0100

    fix: resolve merge conflict in CalculateTimeFin method

--- gestion de salle cinema/src/ClassesBase/Diffusion.java
+++ gestion de salle cinema/src/ClassesBase/Diffusion.java
@@ -32,21 +32,11 @@ public class Diffusion {
             hourfin = this.hourdebut + film.dureeFilm / 60;
         }
     }
-<<<<<<< HEAD
     public void CalculateTimeFin(){
         if(minutedebut+film.dureeFilm%60>60){ 
             minutefin = this.minutedebut - 60 + film.dureeFilm % 60;
             hourfin = this.hourdebut + film.dureeFilm / 60 + 1;
         }else{
-=======
-
-    public void CalculateTimeFin() {
-        if (minutedebut + film.dureeFilm % 60 > 60) {
-
-            minutefin = this.minutedebut - 60 + film.dureeFilm % 60;
-            hourfin = this.hourdebut + film.dureeFilm / 60 + 1;
-        } else {
->>>>>>> fb5b14ea7fdf89668466d003a6e6480ed40f0db8
             minutefin = this.minutedebut + film.dureeFilm % 60;
             hourfin = this.hourdebut + film.dureeFilm / 60;
         }
