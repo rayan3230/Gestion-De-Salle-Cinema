fd280b11359daeee9f4581e7545da7e9cdfd5661
fahd djedi <djedifahd0@gmail.com>
2025-01-31 18:56:37 +0100

    fixing

--- gestion de salle cinema/src/ClassesBase/Diffusion.java
+++ gestion de salle cinema/src/ClassesBase/Diffusion.java
@@ -10,23 +10,29 @@ public class Diffusion {
     public Date date;
     public int hourdebut;
     public int minutedebut;
-    public LocalTime timedebut = LocalTime.of(hourdebut, minutedebut);
+    public LocalTime timedebut = LocalTime.of(this.hourdebut, this.minutedebut);
     public Film film;
     public Salle salle;
     public int hourfin;
     public int minutefin;
     public LocalTime timefin = LocalTime.of(this.hourfin, this.minutefin);
 
-    public Diffusion(Date date, Film film, Salle salle, LocalTime timedebut) {
+    public Diffusion(Date date, Film film, Salle salle, int hourdebut, int minutedebut, int hourfin, int minutefin) {
         this.date = date;
         this.film = film;
         this.salle = salle;
-        this.timedebut = timedebut;
+        this.hourdebut = hourdebut;
+        this.minutedebut = minutedebut;
+        this.hourfin = hourfin;
+        this.minutefin = minutefin;
 
         if (this.minutedebut + film.dureeFilm % 60 > 60) {
+
             this.minutefin = this.minutedebut + film.dureeFilm % 60;
             this.hourfin = this.hourdebut + film.dureeFilm / 60 + 1;
+
         } else {
+
             this.minutefin = this.minutedebut + film.dureeFilm % 60;
             this.hourfin = this.hourdebut + film.dureeFilm / 60;
         }
