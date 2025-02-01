6e9f19cff981c337d34a90b67bac8b1d670bb600
fahd djedi <djedifahd0@gmail.com>
2025-02-01 21:52:09 +0100

    changing design

--- gestion de salle cinema/src/Gui/GuiMain.java
+++ gestion de salle cinema/src/Gui/GuiMain.java
@@ -13,20 +13,20 @@ import javax.swing.border.Border;
 public class GuiMain {
     public static void main(String[] args) {
         // creation d'une bordure
-        Border Bordure = BorderFactory.createLineBorder(Color.gray, 5, true);
+        Border Bordure = BorderFactory.createLineBorder(new Color(75, 75, 100), 5, true);
 
         // Création des Lables
         JLabel OpenningLable = new JLabel("Bienvenue dans l'application de gestion de salle de cinema");
 
         // changement de la couleur du lable
         OpenningLable.setForeground(new java.awt.Color(255, 255, 255));
-        OpenningLable.setBackground(new java.awt.Color(50, 50, 60));
+        OpenningLable.setBackground(new java.awt.Color(25, 25, 45));
         OpenningLable.setOpaque(true);
 
         // Positionnement des Lables
         OpenningLable.setHorizontalAlignment(JLabel.CENTER);
         OpenningLable.setVerticalAlignment(JLabel.CENTER);
-        OpenningLable.setBounds(200, 100, 700, 200);
+        OpenningLable.setBounds(0, 1, 1085, 100);
 
         // Changement de la police et taille du lable
         OpenningLable.setFont(new Font("Arial", Font.BOLD, 20));
@@ -34,9 +34,11 @@ public class GuiMain {
         // Ajout de la bordure
         OpenningLable.setBorder(Bordure);
 
+        // Création de la fenêtre principale
         AppFrame Frame = new AppFrame("Gestion de salle de cinema");
         Frame.setLayout(null);
         Frame.setResizable(false);
+
         // Ajout des Lables
         Frame.add(OpenningLable);
 
