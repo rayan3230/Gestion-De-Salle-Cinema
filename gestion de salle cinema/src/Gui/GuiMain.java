b34155a72537583615674bc82c0d02ea9c1d37c0
fahd djedi <djedifahd0@gmail.com>
2025-02-01 22:34:23 +0100

    MY FIRST PANEL

--- gestion de salle cinema/src/Gui/GuiMain.java
+++ gestion de salle cinema/src/Gui/GuiMain.java
@@ -11,7 +11,11 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 public class GuiMain {
-    public static void main(String[] args) {
+
+    // Création des objets
+
+    // Lable d'ouverture :--------------------------------------------------------
+    private static JLabel CreatOppeningLable() {
         // creation d'une bordure
         Border Bordure = BorderFactory.createLineBorder(new Color(75, 75, 100), 5, true);
 
@@ -26,7 +30,7 @@ public class GuiMain {
         // Positionnement des Lables
         OpenningLable.setHorizontalAlignment(JLabel.CENTER);
         OpenningLable.setVerticalAlignment(JLabel.CENTER);
-        OpenningLable.setBounds(0, 1, 1085, 100);
+        OpenningLable.setBounds(200, 1, 885, 70);
 
         // Changement de la police et taille du lable
         OpenningLable.setFont(new Font("Arial", Font.BOLD, 20));
@@ -34,10 +38,62 @@ public class GuiMain {
         // Ajout de la bordure
         OpenningLable.setBorder(Bordure);
 
+        return OpenningLable;
+    }
+
+    // Lable d'option : --------------------------------------------------------
+    private static JLabel CreatOptionLable() {
+
+        // Création des Lables
+        JLabel OptionLable = new JLabel("App Fuctionalities");
+
+        // changement de la couleur du lable
+        OptionLable.setForeground(new java.awt.Color(255, 255, 255));
+
+        // Positionnement des Lables
+        OptionLable.setHorizontalAlignment(JLabel.CENTER);
+
+        // Changement de la police et taille du lable
+        OptionLable.setFont(new Font("Arial", Font.BOLD, 20));
+
+        // Changement de la police et taille du lable
+        OptionLable.setFont(new Font("Arial", Font.BOLD, 17));
+
+        return OptionLable;
+    }
+
+    // Panel d'option :--------------------------------------------------------
+    private static JPanel CreatOptionPanel() {
+        // creation d'un panel
+        JPanel OptionPanel = new JPanel();
+
+        // changement de la couleur du panel
+        OptionPanel.setBackground(new java.awt.Color(20, 20, 20));
+
+        // changement de la position du panel
+        OptionPanel.setBounds(0, 0, 200, 700);
+
+        // Ajout du text
+        JLabel OptionLable = CreatOptionLable();
+        OptionPanel.add(OptionLable);
+
+        return OptionPanel;
+    }
+
+    // Main--------------------------------------------------------------------
+    public static void main(String[] args) {
+
+        // Création des objets pour les tests
+        JLabel OpenningLable = CreatOppeningLable();
+        JPanel OptionPanel = CreatOptionPanel();
+
         // Création de la fenêtre principale
         AppFrame Frame = new AppFrame("Gestion de salle de cinema");
         Frame.setLayout(null);
         Frame.setResizable(false);
+
+        // Ajout des Panels
+        Frame.add(OptionPanel);
 
         // Ajout des Lables
         Frame.add(OpenningLable);
